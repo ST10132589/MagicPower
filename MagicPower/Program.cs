6c38b76163e8c76514fad313851296461bd4fda2
Blake <ST10132589@vcconnect.edu.za>
2025-02-04 14:26:01 +0200

    Add project files.

---
+++ MagicPower/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MagicPower
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long power = long.Parse(Console.ReadLine());
+            Console.WriteLine(IsMagicalPotion(power));
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+
+        public static string IsMagicalPotion(long power)
+        {
+            long cubeRoot = (long)Math.Round(Math.Pow(power, 1.0 / 3.0));
+            if (cubeRoot * cubeRoot * cubeRoot == power)
+            {
+                return "YES";
+            }
+            return "NO";
+        }
+    }
+}
